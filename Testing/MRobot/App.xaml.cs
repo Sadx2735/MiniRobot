ce63d7844a51dff89af8c992f1b0a4916c644896
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-21 16:33:44 +0530

    First Commit Setting Up things

---
+++ Testing/MRobot/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MRobot {
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App : Application {
+   }
+
+}
