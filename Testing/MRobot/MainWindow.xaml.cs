a445f85f25b4d63e64d8dafbf16403741750791b
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-25 14:27:05 +0530

    Base code to check if stepper works

--- Testing/MRobot/MainWindow.xaml.cs
+++ Testing/MRobot/MainWindow.xaml.cs
@@ -6,16 +6,47 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.IO.Ports;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MRobot {
-   /// <summary>
-   /// Interaction logic for MainWindow.xaml
-   /// </summary>
    public partial class MainWindow : Window {
       public MainWindow () {
          InitializeComponent ();
+         mArduino = new SerialPort ("COM3", 9600);
+         try {
+            mArduino.Open ();
+            StatusLabel.Content = "Connected";
+         } catch (Exception ex) {
+            StatusLabel.Content = "Not connected";
+            MessageBox.Show ("Could not open COM3: " + ex.Message);
+         }
       }
+
+      void ToggleState (object sender, RoutedEventArgs e) {
+         var nState = mMotorState is EState.Running ? EState.Stopped : EState.Running;
+         mMotorState = nState;
+         if (mMotorState is EState.Running) mArduino.WriteLine ("1");
+         else mArduino.WriteLine ("0");
+      }
+
+      void DisconnectPort () {
+         if (mArduino != null && mArduino.IsOpen) {
+            mArduino.Close (); mArduino.Dispose ();
+         }
+      }
+
+      protected override void OnClosed (EventArgs e) {
+         DisconnectPort ();
+         base.OnClosed (e);
+      }
+
+      #region Fields ------------------------------------------------
+      SerialPort mArduino;
+      EState mMotorState = EState.Running;
+      #endregion
    }
 }
+
+public enum EState { Running, Stopped };
